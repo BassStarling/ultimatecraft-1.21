e309671f9944e7749fd61d7de56e6f7218677dcf
BassStraling <bassstarling@outlook.com>
2025-03-08 15:57:54 +0900

    fixed AssemblyBlockEntity

--- src/main/java/net/basssterling/ultimatecraft/block/entity/custom/AssemblyBlockEntity.java
+++ src/main/java/net/basssterling/ultimatecraft/block/entity/custom/AssemblyBlockEntity.java
@@ -16,25 +16,27 @@ import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 
 public class AssemblyBlockEntity extends BlockEntity implements ImplementedInventory {
-    private final DefaultedList<ItemStack> inventory = net.minecraft.util.collection.DefaultedList.ofSize(1,ItemStack.EMPTY);
+    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(1, ItemStack.EMPTY);
+
     public AssemblyBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.ASSEMBLY_BE, pos, state);
     }
 
+    @Override
     public DefaultedList<ItemStack> getItems() {
         return inventory;
-    }
-
-    @Override
-    protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
-        super.readNbt(nbt, registryLookup);
-        Inventories.readNbt(nbt, inventory, registryLookup);
     }
 
     @Override
     protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
         super.writeNbt(nbt, registryLookup);
         Inventories.writeNbt(nbt, inventory, registryLookup);
+    }
+
+    @Override
+    protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        super.readNbt(nbt, registryLookup);
+        Inventories.readNbt(nbt, inventory, registryLookup);
     }
 
     @Nullable
